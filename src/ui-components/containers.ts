ad6dd74771e9417d775fbc928cbcced7cb61df23
Andy Wong <awong@cogolabs.com>
2019-01-22 23:47:58 -0500

    added some visual tweaks

--- src/ui-components/containers.ts
+++ src/ui-components/containers.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import * as c from '../constants/colors';
 import * as d from '../constants/devices';
+import treeBg from '../assets/img/tree.jpg';
 
 export const AppContainer = styled.div`
   height: 100vh;
@@ -8,11 +9,16 @@ export const AppContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  background-color: ${ c.MOCKUP_RED }
+  background-image: url(${treeBg});
+  background-size: cover;
+  background-position: center;
 `
 
 export const ContentContainer = styled.div`
+  color: ${ c.WHITE };
   height: 80vh;
-  width: 80vh;
-  background-color: ${ c.MOCKUP_BLUE };
+  width: 80%;
+  background-color: ${ c.BLACK };
+  opacity: 0.85;
+  box-shadow: 0 6px 10px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
 `
