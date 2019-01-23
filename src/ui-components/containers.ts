bf420af39f43f623cc09d204f7e6c915934a7d0f
Andy Wong <awong@cogolabs.com>
2019-01-22 23:50:41 -0500

    visual tweaks

--- src/ui-components/containers.ts
+++ src/ui-components/containers.ts
@@ -17,7 +17,7 @@ export const AppContainer = styled.div`
 export const ContentContainer = styled.div`
   color: ${ c.WHITE };
   height: 80vh;
-  width: 80%;
+  width: 85vh;
   background-color: ${ c.BLACK };
   opacity: 0.85;
   box-shadow: 0 6px 10px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
